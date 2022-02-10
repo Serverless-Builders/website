aca8f4d91fe8a51c7888716cf408857149e8050f
Johannes Konings <johannes.konings@outlook.com>
2022-02-10 10:25:41 +0000

    add auth

--- pages/header.tsx
+++ pages/header.tsx
@@ -1,41 +1,48 @@
-import type { NextPage } from 'next'
-import { SVGProps } from 'react'
+import { useState } from 'react'
+import { NextPage } from 'next'
+import { Amplify, Auth } from 'aws-amplify';
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
 
-const navigation = [
-    { name: 'Solutions', href: '#' },
-    { name: 'Pricing', href: '#' },
-    { name: 'Docs', href: '#' },
-    { name: 'Company', href: '#' },
-]
+import awsconfig from './../src/aws-exports';
+Amplify.configure({
+    ...awsconfig,
+    ssr: true
+});
+
 
 const Header: NextPage = () => {
+    const [showAuth, setShowAuth] = useState(false);
+
     return (<header className="bg-gray-800">
         <nav className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8" aria-label="Top">
             <div className="w-full py-6 flex items-center justify-between border-b border-indigo-500 lg:border-none">
                 <div className="flex items-center">
                 </div>
                 <div className="ml-10 space-x-4">
-                    <a
-                        href="#"
+                    <button
+                        onClick={() => {
+                            setShowAuth(showAuth ? false : true)
+                        }}
+                        type="button"
                         className="inline-block bg-indigo-500 py-2 px-4 border border-transparent rounded-md text-base font-medium text-white hover:bg-opacity-75"
                     >
-                        Sign in
-                    </a>
-                    <a
-                        href="#"
-                        className="inline-block bg-white py-2 px-4 border border-transparent rounded-md text-base font-medium text-indigo-600 hover:bg-indigo-50"
-                    >
-                        Sign up
-                    </a>
+                        Login Toggle
+                    </button>
+                    {showAuth ? (<Authenticator>
+                        {({ signOut, user }) => (
+                            <main>
+                                <p className="mt-2 text-lg font-medium text-white text-opacity-50">
+                                    Hello {user.username}
+                                </p>
+                                <button
+                                    className="inline-block bg-indigo-500 py-2 px-4 border border-transparent rounded-md text-base font-medium text-white hover:bg-opacity-75"
+                                    onClick={signOut}>Sign out</button>
+                            </main>
+                        )}
+                    </Authenticator>) : null}
                 </div>
             </div>
-            {/* <div className="py-4 flex flex-wrap justify-center space-x-6 lg:hidden">
-                {navigation.map((link) => (
-                    <a key={link.name} href={link.href} className="text-base font-medium text-white hover:text-indigo-50">
-                        {link.name}
-                    </a>
-                ))}
-            </div> */}
         </nav>
     </header>)
 }
